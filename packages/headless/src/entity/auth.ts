9a543b42906f523df4dc7e9c220fc8bef788f620
Fahad Jabbar <fahadjabbar31@gmail.com>
2023-11-19 02:27:35 +0500

    Initialize seeder-data

---
+++ packages/headless/src/entity/auth.ts
@@ -0,0 +1,11 @@
+export class SignInData {
+  username: string;
+  password: string;
+}
+
+export class AdminJwtPayload {
+  id: string;
+  username: string;
+  logInTime: number;
+  role: string;
+}
