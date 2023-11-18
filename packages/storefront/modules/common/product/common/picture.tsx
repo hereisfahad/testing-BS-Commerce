9f08f56ed089db64911358ddbec1d9c0f6ff815f
Fahad Jabbar <fahadjabbar31@gmail.com>
2023-11-19 02:27:31 +0500

    Initialize feature/headless-config-alies

---
+++ packages/storefront/modules/common/product/common/picture.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/legacy/image';
+import myImageLoader from 'image/loader';
+
+import React from 'react';
+interface props {
+  height: number;
+  width: number;
+  src: string;
+  alt: string;
+}
+const Picture = ({ height, width, src, alt }: props) => {
+  return (
+    <Image
+      loader={myImageLoader}
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+    />
+  );
+};
+
+export default Picture;
