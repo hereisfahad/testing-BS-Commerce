344cddd48e3fc1747f96b3d3603cf52e49f35eb4
Fahad Jabbar <fahadjabbar31@gmail.com>
2023-11-19 02:28:16 +0500

    Initialize development

---
+++ packages/headless/src/database/mysql/brand/brand.model.ts
@@ -0,0 +1,23 @@
+import { randomUUID } from 'crypto';
+import { Column, Model, Table, HasOne } from 'sequelize-typescript';
+
+import { Brand } from '../../../entity/brand';
+import InfoModel from './info.model';
+import MetaModel from './meta.model';
+
+@Table({
+  tableName: 'Brands',
+})
+export default class BrandModel extends Model<Brand> {
+  @Column({
+    primaryKey: true,
+    defaultValue: () => randomUUID(),
+  })
+  id: string;
+
+  @HasOne(() => InfoModel)
+  info: InfoModel;
+
+  @HasOne(() => MetaModel)
+  meta: MetaModel;
+}
---
+++ packages/storefront/pages/checkout.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import CheckoutComponent from '@/modules/checkout';
+
+const CheckoutPage: NextPage = () => {
+  return <CheckoutComponent />;
+};
+
+export default CheckoutPage;
