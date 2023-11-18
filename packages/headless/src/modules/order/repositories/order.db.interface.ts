91b9bc891337aeac0dbb3a62a30196f7b02ff1bc
Fahad Jabbar <fahadjabbar31@gmail.com>
2023-11-19 02:26:23 +0500

    Initialize headless/tahsin/review/create-rating

---
+++ packages/headless/src/modules/order/repositories/order.db.interface.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import {
+  CreateOrderRequest,
+  CreateProductOrderDetails,
+} from '@bs-commerce/models';
+
+import {
+  CartItem,
+  Cart,
+  CartResponse,
+  ChangeStatusEntity,
+  GetAllOrderQueryEntity,
+  OrderEntity,
+  OrderIncompleteStatEntity,
+  OrderSortQuery,
+  OrderStatEntity,
+  ProductOrder,
+} from 'src/entity/order';
+import { CreateReviewResponse } from 'models';
+import { Review } from 'src/entity/review';
+
+@Injectable()
+export abstract class IOrderDatabase {
+  abstract createOrder: (
+    userId: string,
+    body: CreateOrderRequest,
+  ) => Promise<OrderEntity>;
+  abstract addPhotoDetails: (
+    products: CreateProductOrderDetails[],
+  ) => Promise<ProductOrder[]>;
+  abstract getOrderListByUserId: (
+    userId: string,
+    sortObj: OrderSortQuery,
+  ) => Promise<OrderEntity[]>;
+  abstract findOrder: (query: Record<string, any>) => Promise<OrderEntity>;
+  abstract getOrderStatistics: () => Promise<OrderStatEntity>;
+  abstract getIncompleteStatistics: () => Promise<OrderIncompleteStatEntity>;
+  abstract changeStatus: (body: ChangeStatusEntity) => Promise<OrderEntity>;
+  abstract getOrderList: (
+    query?: GetAllOrderQueryEntity,
+    skip?: number,
+    limit?: number,
+  ) => Promise<OrderEntity[]>;
+  abstract getAvailableProducts: (productIds: string[]) => Promise<any>;
+  abstract getCart: (userId: string) => Promise<Cart | null>;
+  abstract populateItemsInCart: (
+    userId: string,
+    items: CartItem[],
+  ) => Promise<CartResponse | null>;
+  abstract clearCart: (userId: string) => Promise<CartResponse | null>;
+  abstract createReview: (review: any) => Promise<Review | null>;
+  abstract findReview: (query: Record<string,any>, skip: number, limit: number)=> Promise<Review[] | null>;
+  abstract addProductRating: (productId: string, rating: number)=> Promise<boolean>;
+}
