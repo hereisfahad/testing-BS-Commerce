663d3ec77295d1ae005013519d60d67f89957dbd
Fahad Jabbar <fahadjabbar31@gmail.com>
2023-11-19 02:22:53 +0500

    Initialize headless/bsdhrubo/BC-611/development/seeder-improvement

---
+++ packages/seeder/src/data/index.ts
@@ -0,0 +1,7 @@
+export * from './products' 
+export * from './brands' 
+export * from './categories' 
+export * from './manufacturers' 
+export * from './tags' 
+export * from './admins' 
+export * from './customers' 
---
+++ packages/seeder/src/seeder.ts
@@ -0,0 +1,69 @@
+import * as fs from 'fs'
+import { Model } from 'mongoose'
+import * as path from 'path'
+import UserModel from './admin/admin.model'
+import BrandModel from './brand/brand.model'
+import CategoryModel from './category/category.model'
+import CustomerModel from './customer/customer.model'
+import { admins, brands, categories, customers, manufacturers, products, tags } from './data'
+import ManufacturerModel from './manufacturer/manufacturer.model'
+import { connectToDatabase } from './mongodb.connect'
+import ProductModel from './product/product.model'
+import TagsModel from './tags/tags.model'
+
+const run = async ()=>{
+    await connectToDatabase();
+    await getDataFiles()
+}
+
+// get list of data file
+const getDataFiles = async ()=>{
+   const fileList = fs.readdirSync('src/data')
+   
+   fileList.forEach( async file=>{
+    if(path.parse(file).ext === '.ts'){
+        const name = path.parse(file).name
+        switch(name){
+            case 'products': 
+                await inserData(ProductModel, products)
+                break;
+            case 'brands':
+                await inserData(BrandModel, brands)
+                break;
+            case 'categories':
+                await inserData(CategoryModel, categories)
+                break;
+            case 'manufacturers':
+                await inserData(ManufacturerModel, manufacturers)
+                break;
+            case 'tags':
+                await inserData(TagsModel, tags)
+                break;
+            case 'admins':
+                await inserData(UserModel, admins)
+                break;
+            case 'customers':
+                await inserData(CustomerModel, customers)
+                break;
+            default:
+                console.log(name, 'Data file not matched')
+        }
+    }
+   }) 
+}
+
+const inserData = async <T>(model: Model<T>, data: any)=>{
+    await model.collection.drop()
+    const mapped =  data.map(e =>{
+        delete e?._id
+        delete e?.createdAt
+        delete e?.updatedAt
+        return e
+    })
+ 
+    await model.insertMany(mapped)
+    console.log(model.collection.name,'data inserted.')
+}
+
+
+run()
