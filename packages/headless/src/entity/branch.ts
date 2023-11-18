aaf8335111c513d61f5ab66dc5022563b9086f72
Fahad Jabbar <fahadjabbar31@gmail.com>
2023-11-19 02:26:33 +0500

    Initialize headless/tahsin/multi-vendor-backend/merge-issue-fix

---
+++ packages/headless/src/entity/branch.ts
@@ -0,0 +1,30 @@
+export class BranchAddress{
+    id: string;
+    addressLine1: string;
+    addressLine2?: string;
+    postCode: string;
+    city: string;
+    country: string;
+}
+
+export class BranchPhoto{
+    cover?: string;
+    logo?: string;
+}
+
+export enum InActiveReason{
+    BLOCKED_BY_ADMIN = 'BLOCKED_BY_ADMIN',
+    UNDER_MAINTENANCE = 'UNDER_MAINTENANCE',
+    ADMIN_APPROVAL = 'NEED ADMIN APPROVAL'
+}
+export class Branch{
+    id: string;
+    store: string; //storeId
+    url: string;
+    address: BranchAddress;
+    isActive: boolean;
+    inActiveReason?: InActiveReason;
+    image?: BranchPhoto;
+    description?: string;
+    name: string;
+}
---
+++ packages/superadmin/next.config.js
@@ -0,0 +1,34 @@
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    domains: [
+      'admin-demo.nopcommerce.com',
+      'cdn.shopify.com',
+      'chaldn.com',
+      'i2.wp.com',
+      'upload.wikimedia.org',
+      'image.made-in-china.com',
+      'bunonbasket.com',
+      'img.freepik.com',
+      'm.media-amazon.com',
+      'd2j6dbq0eux0bg.cloudfront.net',
+      'www.gardeningknowhow.com',
+      'www.aprifel.com',
+      'urbanbazaar.com.np',
+      'helios-i.mashable.com',
+      'www.hindustantimes.com',
+    ],
+  },
+};
+
+const withTM = require('next-transpile-modules')(['marketplace']);
+module.exports = withTM(nextConfig);
