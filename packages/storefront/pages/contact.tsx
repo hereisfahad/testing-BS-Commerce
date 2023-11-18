af8a78172f568ba157bb6d3fd06a4e4059b557a1
Fahad Jabbar <fahadjabbar31@gmail.com>
2023-11-19 02:27:41 +0500

    Initialize relative-path-fix

---
+++ packages/storefront/pages/contact.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import ContactComponent from '@/modules/static/contact';
+
+const Contact: NextPage = () => {
+  return (
+    <>
+      <ContactComponent />
+    </>
+  );
+};
+
+export default Contact;
---
+++ packages/storefront/pages/faq.tsx
@@ -0,0 +1,8 @@
+import FaqComponent from '@/modules/static/faq';
+import type { NextPage } from 'next';
+
+const Faq: NextPage = () => {
+  return <FaqComponent />;
+};
+
+export default Faq;
