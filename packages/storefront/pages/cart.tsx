df8d8060b1631e9289530e76a30fabe156edf4ac
Fahad Jabbar <fahadjabbar31@gmail.com>
2023-11-19 02:26:38 +0500

    Initialize storefront-restructure-himel

---
+++ packages/storefront/pages/cart.tsx
@@ -0,0 +1,44 @@
+import type { GetServerSideProps, NextPage } from 'next';
+
+import CartComponent from '@/modules/cart/index';
+import { Cart, ResponseItem } from '@bs-commerce/models';
+import { userAPI } from 'APIs';
+import { useAppDispatch } from 'store/hooks';
+import { storeAllCartItems } from 'store/slices/cartSlice';
+import { useEffect } from 'react';
+var cookie = require('cookie');
+
+interface Props {
+  cartProducts: ResponseItem[];
+}
+
+const Cart: NextPage<Props> = ({ cartProducts }: Props) => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(storeAllCartItems(cartProducts));
+  }, [dispatch, cartProducts]);
+
+  return (
+    <>
+      <div>
+        <CartComponent />
+      </div>
+    </>
+  );
+};
+
+export default Cart;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const reqCookie = context.req.headers.cookie;
+  const token = reqCookie === undefined ? undefined : cookie.parse(reqCookie);
+  let response;
+  if (reqCookie) {
+    response = await userAPI.getCart(token.token);
+  }
+  return {
+    props: {
+      cartProducts: response?.data?.items!,
+    },
+  };
+};
