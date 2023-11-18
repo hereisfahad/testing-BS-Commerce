60ee133491c8bb9461d0962a061f3e6695c74b59
Fahad Jabbar <fahadjabbar31@gmail.com>
2023-11-19 02:25:08 +0500

    Initialize storefront-structure-backup

---
+++ packages/storefront/pages/deals.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import DealsComponent from '@/modules/deals/index';
+
+const Deals: NextPage = () => {
+  return (
+    <>
+      <div>
+        <DealsComponent />
+      </div>
+    </>
+  );
+};
+
+export default Deals;
---
+++ packages/storefront/pages/order/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+// import OrderMain from '@/modules/order/ordersList/index';
+import Orders from '@/modules/order/orders';
+
+const Order: NextPage = () => {
+  return (
+    <>
+      <Orders />
+    </>
+  );
+};
+
+export default Order;
